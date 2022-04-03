cd23f968af232f66e097a85ad51167c1aab06f08
DZWG <paule22@protonmail.com>
2022-04-03 16:32:57 +0200

    updated image + check if API is up

--- src/main.rs
+++ src/main.rs
@@ -76,19 +76,25 @@ struct General;
 
 #[tokio::main]
 async fn main() {
-    // This will load the environment variables located at `./.env`, relative to
-    // the CWD. See `./.env.example` for an example on how to structure this.
-    //dotenv::dotenv().expect("Failed to load .env file");
-
-
-    // Initialize the logger to use environment variables.
-    //
-    // In this case, a good default is setting the environment variable
-    // `RUST_LOG` to `debug`.
     tracing_subscriber::fmt::init();
 
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    //Make sure API is up before continuing
+    loop {
+        match api_helper::get_raffle("0".to_owned()).await {
+            Ok(_) => {
+                info!("API-Connected gonna continue");
+                break
+            },
+            Err(e) => {
+                error!("Unable to connect to API: {}",e);
+                info!("Retrying after 10s");
+                tokio::time::sleep(Duration::from_secs(10)).await;}
+        };
+    }
 
+
+
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     let http = Http::new_with_token(&token);
 
     // We will fetch your bot's owners and id
